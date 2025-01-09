db0e576011396e9f042a2554db7488364cf85051
Alex Li <github@alexv525.com>
2025-01-10 00:04:13 +0800

    💡 Improve location classes

--- lib/src/location.dart
+++ lib/src/location.dart
@@ -1,49 +1,17 @@
 part of x_amap_base;
 
-///定位信息类
+/// 定位信息类
 ///
-///可提供内容包括：
+/// 可提供内容包括：
 ///
-///[provider]定位信息提供者，注意，如果是iOS平台只会返回‘iOS’
-///
-///[latLng]经纬度信息
-///
-///[accuracy] 水平精确度
-///
-///[altitude] 海拔
-///
-///[bearing] 角度
-///
-///[speed] 速度
-///
-///[time] 定位时间
-///
+/// [provider]定位信息提供者，注意，如果是iOS平台只会返回‘iOS’
+/// [latLng]经纬度信息
+/// [accuracy] 水平精确度
+/// [altitude] 海拔
+/// [bearing] 角度
+/// [speed] 速度
+/// [time] 定位时间
 class AMapLocation {
-  ///定位提供者
-  ///
-  ///Android平台根据系统信息透出
-  ///
-  ///iOS平台只会返回'iOS'
-  final String provider;
-
-  ///经纬度
-  final LatLng latLng;
-
-  ///水平精确度
-  final double accuracy;
-
-  ///海拔
-  final double altitude;
-
-  ///角度
-  final double bearing;
-
-  ///速度
-  final double speed;
-
-  ///定位时间
-  final num time;
-
   const AMapLocation({
     this.provider = '',
     required this.latLng,
@@ -53,6 +21,30 @@ class AMapLocation {
     this.speed = 0,
     this.time = 0,
   });
+
+  /// 定位提供者
+  ///
+  /// Android平台根据系统信息透出
+  /// iOS平台只会返回'iOS'
+  final String provider;
+
+  /// 经纬度
+  final LatLng latLng;
+
+  /// 水平精确度
+  final double accuracy;
+
+  /// 海拔
+  final double altitude;
+
+  /// 角度
+  final double bearing;
+
+  /// 速度
+  final double speed;
+
+  /// 定位时间
+  final num time;
 
   static AMapLocation? fromMap(dynamic json) {
     if (null == json) {
@@ -71,10 +63,10 @@ class AMapLocation {
   }
 
   /// Converts this object to something serializable in JSON.
-  dynamic toJson() {
-    final Map<String, dynamic> json = <String, dynamic>{};
+  Map<String, Object> toJson() {
+    final json = <String, Object>{};
 
-    void addIfPresent(String fieldName, dynamic value) {
+    void addIfPresent(String fieldName, Object? value) {
       if (value != null) {
         json[fieldName] = value;
       }
@@ -92,36 +84,48 @@ class AMapLocation {
 
   @override
   bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-    if (other.runtimeType != runtimeType) return false;
+    if (identical(this, other)) {
+      return true;
+    }
     if (other is! AMapLocation) {
       return false;
     }
-    final AMapLocation typedOther = other;
-    return provider == typedOther.provider &&
-        latLng == typedOther.latLng &&
-        accuracy == typedOther.accuracy &&
-        altitude == typedOther.altitude &&
-        bearing == typedOther.bearing &&
-        speed == typedOther.speed &&
-        time == typedOther.time;
+    return provider == other.provider &&
+        latLng == other.latLng &&
+        accuracy == other.accuracy &&
+        altitude == other.altitude &&
+        bearing == other.bearing &&
+        speed == other.speed &&
+        time == other.time;
   }
 
   @override
-  int get hashCode => Object.hashAll(
-      [provider, latLng, accuracy, altitude, bearing, speed, time]);
+  int get hashCode => Object.hashAll([
+        provider,
+        latLng,
+        accuracy,
+        altitude,
+        bearing,
+        speed,
+        time,
+      ]);
 }
 
-/// 经纬度坐标对象， 单位为度.
+/// 经纬度坐标对象，单位为度。
+@immutable
 class LatLng {
-  ///根据纬度[latitude]和经度[longitude]创建经纬度对象
+  /// 根据纬度 [latitude] 和经度 [longitude] 创建经纬度对象。
   ///
-  ///[latitude]取值范围 [-90.0,90.0].
-  ///
-  /// [latitude]取值范围 [-180.0,179.0]
-  const LatLng(double latitude, double longitude)
-      : latitude =
-            (latitude < -90.0 ? -90.0 : (90.0 < latitude ? 90.0 : latitude)),
+  /// [latitude] 取值范围 [-90.0, 90.0]
+  /// [longitude] 取值范围 [-180.0, 180.0]
+  const LatLng(
+    double latitude,
+    double longitude,
+  )   : latitude = latitude < -90.0
+            ? -90.0
+            : 90.0 < latitude
+                ? 90.0
+                : latitude,
         longitude = (longitude + 180.0) % 360.0 - 180.0;
 
   /// 纬度
@@ -130,41 +134,48 @@ class LatLng {
   /// 经度
   final double longitude;
 
-  dynamic toJson() {
-    return <double>[latitude, longitude];
-  }
-
   /// 根据传入的经纬度数组 \[lat, lng\] 序列化一个LatLng对象.
-  static LatLng? fromJson(dynamic json) {
+  static LatLng? fromJson(List? json) {
     if (json == null) {
       return null;
     }
     return LatLng(json[0], json[1]);
   }
 
-  @override
-  String toString() => '$runtimeType($latitude, $longitude)';
+  List<double> toJson() {
+    return <double>[latitude, longitude];
+  }
 
   @override
-  bool operator ==(Object o) {
-    return o is LatLng && o.latitude == latitude && o.longitude == longitude;
+  String toString() => 'LatLng(${toJson()})';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (other is! LatLng) {
+      return false;
+    }
+    return other.latitude == latitude && other.longitude == longitude;
   }
 
   @override
   int get hashCode => Object.hashAll([latitude, longitude]);
 }
 
-/// 经纬度对齐的矩形.
+/// 代表了经纬度划分的一个矩形区域。
+@immutable
 class LatLngBounds {
-  /// 使用传入的西南角坐标[southwest]和东北角坐标[northeast]创建一个矩形区域.
-  LatLngBounds({required this.southwest, required this.northeast}) {
-    try {
-      assert(southwest.latitude <= northeast.latitude,
-          '西南角纬度超过了东北角纬度(${southwest.latitude} > ${northeast.latitude})');
-    } catch (e) {
-      print(e);
-    }
-  }
+  /// 使用传入的西南角坐标 [southwest] 和东北角坐标 [northeast] 创建一个矩形区域。
+  LatLngBounds({
+    required this.southwest,
+    required this.northeast,
+  }) : assert(
+          southwest.latitude <= northeast.latitude,
+          '西南角纬度超过了东北角纬度'
+          '(${southwest.latitude} > ${northeast.latitude})',
+        );
 
   /// 西南角坐标.
   final LatLng southwest;
@@ -172,22 +183,20 @@ class LatLngBounds {
   /// 东北角坐标.
   final LatLng northeast;
 
-  dynamic toJson() {
-    if (southwest.latitude > northeast.latitude) {
+  static LatLngBounds? fromList(List? json) {
+    if (json == null) {
       return null;
     }
-    return <dynamic>[southwest.toJson(), northeast.toJson()];
+    return LatLngBounds(
+      southwest: LatLng.fromJson(json[0])!,
+      northeast: LatLng.fromJson(json[1])!,
+    );
   }
 
   /// 判断矩形区域是否包含传入的经纬度[point].
   bool contains(LatLng point) {
-    try {
-      return _containsLatitude(point.latitude) &&
-          _containsLongitude(point.longitude);
-    } catch (e) {
-      print(e);
-    }
-    return false;
+    return _containsLatitude(point.latitude) &&
+        _containsLongitude(point.longitude);
   }
 
   bool _containsLatitude(double lat) {
@@ -202,27 +211,20 @@ class LatLngBounds {
     }
   }
 
-  @visibleForTesting
-  static LatLngBounds? fromList(dynamic json) {
-    if (json == null) {
-      return null;
-    }
-    return LatLngBounds(
-      southwest: LatLng.fromJson(json[0])!,
-      northeast: LatLng.fromJson(json[1])!,
-    );
+  List<List<double>> toJson() {
+    return [southwest.toJson(), northeast.toJson()];
   }
 
   @override
   String toString() {
-    return '$runtimeType($southwest, $northeast)';
+    return 'LatLngBounds(${toJson()})';
   }
 
   @override
-  bool operator ==(Object o) {
-    return o is LatLngBounds &&
-        o.southwest == southwest &&
-        o.northeast == northeast;
+  bool operator ==(Object other) {
+    return other is LatLngBounds &&
+        other.southwest == southwest &&
+        other.northeast == northeast;
   }
 
   @override
